33e8e5743ce67a7a409cca3f2765ddd0c84042ad
riya0309 <123491482+riya0309@users.noreply.github.com>
2023-06-06 17:43:48 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var ground;
+
+let engine;
+let world;
+
+function setup() 
+{
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+ 
+  ground = new Ground(200,680,600,20);
+
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+}
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+
+  ground.show();
+   
+}
+
+
+
+
